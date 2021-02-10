f6a3cbda42863ee8cd467d25edc97bae0880744a
Yefenny Chireno <yefenny300@icloud.com>
2021-02-10 12:09:22 -0500

    change postgrator and server to make heroku work

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+const pg = require('pg');
+pg.defaults.ssl =
+  process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
