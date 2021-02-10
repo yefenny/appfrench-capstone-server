da3e869c1a407cb646b3f88748b0c39f441dee4e
Yefenny Chireno <yefenny300@icloud.com>
2021-02-10 12:29:11 -0500

    change postgrator

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,10 +7,15 @@ pg.defaults.ssl =
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  host: process.env.MIGRATION_DB_HOST,
-  port: process.env.MIGRATION_DB_PORT,
-  database: process.env.MIGRATION_DB_NAME,
-  username: process.env.MIGRATION_DB_USER,
-  password: process.env.MIGRATION_DB_PASS,
+  // host: process.env.MIGRATION_DB_HOST,
+  // port: process.env.MIGRATION_DB_PORT,
+  // database: process.env.MIGRATION_DB_NAME,
+  // username: process.env.MIGRATION_DB_USER,
+  // password: process.env.MIGRATION_DB_PASS,
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+
   SSL: true
 };
